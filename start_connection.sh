908afb3d3f032a13fbbd634f1fc0022e33997141
p4p1 <p4p1@vivaldi.net>
2021-03-15 11:42:30 +0100

    added start_script

---
+++ start_connection.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# start_connection.sh
+# Created on: Mon 15 Mar 2021 11:42:16 AM CET
+#
+#  ____   __  ____  __
+# (  _ \ /. |(  _ \/  )
+#  )___/(_  _))___/ )(
+# (__)    (_)(__)  (__)
+#
+# Description:
+
+sudo systemctl start wg-quick@wg0
